6ed0f75856e5c996291eb6e1d2c7bba081772ffa
Oscar I. Rojas <oscarrojas991@protonmail.com>
2026-03-25 09:11:20 -0600

    Completed JavaScript basics Assignment 01 - Variables and Operators

---
+++ foundations/JavaScript_Basics/Assignments/01-VariablesAndOperators/main.js
@@ -0,0 +1,48 @@
+// let firstName = "John";
+// let lastName = "Doe";
+//
+//
+// console.log(firstName);
+// console.log(lastName);
+//
+//
+// let age = 11;
+//
+// console.log(age);
+//
+// age = 54;
+//
+// console.log(age);
+//
+//
+// const pi = 3.14;
+//
+// pi = 10;
+//
+// console.log(pi);
+
+
+// ASIGNMENT
+
+// #1
+console.log(23 + 97);
+
+// #2
+console.log(87 + 983256 + 2 + 10 + 34 + 1);
+
+// #3
+console.log((4 + 6 + 9) / 77);
+
+// #4
+let a = 10;
+console.log(a);
+a = 5;
+console.log(a);
+let b = a * 7;
+console.log(b);
+
+// #5
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log("Percentage: " + percentage);
